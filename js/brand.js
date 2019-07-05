e95391fc4950f1f385942c972a95c45c9cb9796c
gongqia <1192991541@qq.com>
2019-07-05 10:14:30 +0800

    Sephora commit

---
+++ js/brand.js
@@ -0,0 +1,779 @@
+var headerList =[{
+		"title":"热门",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"功效",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"护肤",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"彩妆",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"香水",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"工具",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"男士护肤",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"洗浴护体",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	},
+	{
+		"title":"美发护发",
+		"t1":"魅惑美唇",
+		"t2":"塑造立体轮廓",
+		"t3":"热销气垫"
+	}]
+
+var brand = [{
+		"src": "img/index/brand_icon1.png",
+		"name": "丝芙兰"
+	},
+	{
+		"src": "img/index/brand_icon2.png",
+		"name": "兰蔻"
+	},
+	{
+		"src": "img/index/brand_icon3.png",
+		"name": "彼得罗夫"
+	},
+	{
+		"src": "img/index/brand_icon4.png",
+		"name": "纪梵希"
+	},
+	{
+		"src": "img/index/brand_icon5.png",
+		"name": "黎珐"
+	},
+	{
+		"src": "img/index/brand_icon6.png",
+		"name": "芭比波朗"
+	},
+	{
+		"src": "img/index/brand_icon7.png",
+		"name": "雅诗兰黛"
+	},
+	{
+		"src": "img/index/brand_icon8.png",
+		"name": "SK-II"
+	},
+	{
+		"src": "img/index/brand_icon9.png",
+		"name": "罗意威"
+	},
+	{
+		"src": "img/index/brand_icon10.png",
+		"name": "蔚蓝之美"
+	},
+	{
+		"src": "img/index/brand_icon11.png",
+		"name": "馥蕾诗"
+	},
+	{
+		"src": "img/index/brand_icon12.png",
+		"name": "娇兰"
+	},
+	{
+		"src": "img/index/brand_icon13.png",
+		"name": "兰贾斯汀"
+	},
+	{
+		"src": "img/index/brand_icon14.png",
+		"name": "玛丽黛佳玩色系列"
+	},
+	{
+		"src": "img/index/brand_icon15.png",
+		"name": "迪奥"
+	},
+	{
+		"src": "img/index/brand_icon16.png",
+		"name": "MaisonMargiela"
+	}]
+
+var brand01 = [{
+		"src1": "img/index/brand01_img1.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻新精华肌底液",
+		"price": "￥760",
+		"src2":"img/index/brand01_spic1.jpg"
+	},
+	{
+		"src1": "img/index/brand01_img1.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻新精华肌底液",
+		"price": "￥760",
+		"src2":"img/index/brand01_spic1.jpg"
+	},
+	{
+		"src1": "img/index/brand01_img1.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻新精华肌底液",
+		"price": "￥760",
+		"src2":"img/index/brand01_spic1.jpg"
+	}]
+
+
+var brand02 = [{
+		"src": "img/index/brand02_spic1.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻肌底精华眼霜嘉年华礼...",
+		"price": "￥520.00"
+	},
+	{
+		"src": "img/index/brand02_spic2.jpg",
+		"name1": "LANEIGE",
+		"name2": "兰芝水衡清盈修护套装",
+		"price": "￥625.00"
+	},
+	{
+		"src": "img/index/brand02_spic3.jpg",
+		"name1": "ESTEELAUDER",
+		"name2": "新客专享|雅诗兰黛特润修...",
+		"price": "￥510.00"
+	},
+	{
+		"src": "img/index/brand02_spic4.jpg",
+		"name1": "LANCASTER",
+		"name2": "兰嘉丝汀悦阳舒爽倍护娇容...",
+		"price": "￥220.00"
+	},
+	{
+		"src": "img/index/brand02_spic5.jpg",
+		"name1": "PETERTHOMASROTH",
+		"name2": "彼得罗夫青瓜舒缓补水惠选...",
+		"price": "￥360.00"
+	},
+	{
+		"src": "img/index/brand02_spic6.jpg",
+		"name1": "ReFa",
+		"name2": "ReFa经典焕颜美容仪套...",
+		"price": "￥1890.00"
+	},
+	{
+		"src": "img/index/brand02_spic7.jpg",
+		"name1": "BIOTHERM",
+		"name2": "碧欧泉奇迹水清透礼盒",
+		"price": "￥490.00"
+	},
+	{
+		"src": "img/index/brand02_spic8.jpg",
+		"name1": "Fresh",
+		"name2": "馥蕾诗玫瑰润泽随行礼盒",
+		"price": "￥625.00"
+	}]
+
+var brand03 = [{
+		"src": "img/index/brand03_spic1.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥烈艳蓝金挚红...",
+		"price": "￥320.00"
+	},
+	{
+		"src": "img/index/brand03_spic2.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥烈艳蓝金挚红...",
+		"price": "￥320.00"
+	},
+	{
+		"src": "img/index/brand03_spic3.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥烈艳蓝金挚红...",
+		"price": "￥320.00"
+	},
+	{
+		"src": "img/index/brand03_spic4.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥烈艳蓝金挚红...",
+		"price": "￥320.00"
+	},
+	{
+		"src": "img/index/brand03_spic5.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥烈艳蓝金挚红...",
+		"price": "￥320.00"
+	},
+	{
+		"src": "img/index/brand03_spic6.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥烈艳蓝金挚红...",
+		"price": "￥320.00"
+	},
+	{
+		"src": "img/index/brand03_spic7.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥烈艳蓝金挚红...",
+		"price": "￥320.00"
+	},
+	{
+		"src": "img/index/brand03_spic8.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥烈艳蓝金挚红...",
+		"price": "￥320.00"
+	}]
+
+var brand04 = [{
+		"src": "img/index/brand04_spic1.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥迪奥小姐随行...",
+		"price": "￥380.00~￥450.00"
+	},
+	{
+		"src": "img/index/brand04_spic2.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥迪奥小姐随行...",
+		"price": "￥380.00~￥450.00"
+	},
+	{
+		"src": "img/index/brand04_spic3.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥迪奥小姐随行...",
+		"price": "￥380.00~￥450.00"
+	},
+	{
+		"src": "img/index/brand04_spic4.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥迪奥小姐随行...",
+		"price": "￥380.00~￥450.00"
+	},
+	{
+		"src": "img/index/brand04_spic5.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥迪奥小姐随行...",
+		"price": "￥380.00~￥450.00"
+	},
+	{
+		"src": "img/index/brand04_spic6.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥迪奥小姐随行...",
+		"price": "￥380.00~￥450.00"
+	},
+	{
+		"src": "img/index/brand04_spic7.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥迪奥小姐随行...",
+		"price": "￥380.00~￥450.00"
+	},
+	{
+		"src": "img/index/brand04_spic8.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥迪奥小姐随行...",
+		"price": "￥380.00~￥450.00"
+	}]
+
+var brand05 = [{
+		"src": "img/index/brand05_spic1.jpg",
+		"name1": "LABSERIES",
+		"name2": "LAB SERIES朗仕...",
+		"price": "￥790.00"
+	},
+	{
+		"src": "img/index/brand05_spic2.jpg",
+		"name1": "LABSERIES",
+		"name2": "LAB SERIES朗仕...",
+		"price": "￥790.00"
+	},
+	{
+		"src": "img/index/brand05_spic3.jpg",
+		"name1": "LABSERIES",
+		"name2": "LAB SERIES朗仕...",
+		"price": "￥790.00"
+	},
+	{
+		"src": "img/index/brand05_spic4.jpg",
+		"name1": "LABSERIES",
+		"name2": "LAB SERIES朗仕...",
+		"price": "￥790.00"
+	},
+	{
+		"src": "img/index/brand05_spic5.jpg",
+		"name1": "LABSERIES",
+		"name2": "LAB SERIES朗仕...",
+		"price": "￥790.00"
+	},
+	{
+		"src": "img/index/brand05_spic6.jpg",
+		"name1": "LABSERIES",
+		"name2": "LAB SERIES朗仕...",
+		"price": "￥790.00"
+	},
+	{
+		"src": "img/index/brand05_spic7.jpg",
+		"name1": "LABSERIES",
+		"name2": "LAB SERIES朗仕...",
+		"price": "￥790.00"
+	},
+	{
+		"src": "img/index/brand05_spic8.jpg",
+		"name1": "LABSERIES",
+		"name2": "LAB SERIES朗仕...",
+		"price": "￥790.00"
+	}]
+
+var brand06 = [{
+		"src": "img/index/brand06_spic1.jpg",
+		"name1": "Fresh",
+		"name2": "馥蕾诗保湿修护唇膜（柠檬...",
+		"price": "￥150.00"
+	},
+	{
+		"src": "img/index/brand06_spic2.jpg",
+		"name1": "Fresh",
+		"name2": "馥蕾诗保湿修护唇膜（柠檬...",
+		"price": "￥150.00"
+	},
+	{
+		"src": "img/index/brand06_spic3.jpg",
+		"name1": "Fresh",
+		"name2": "馥蕾诗保湿修护唇膜（柠檬...",
+		"price": "￥150.00"
+	},
+	{
+		"src": "img/index/brand06_spic4.jpg",
+		"name1": "Fresh",
+		"name2": "馥蕾诗保湿修护唇膜（柠檬...",
+		"price": "￥150.00"
+	},
+	{
+		"src": "img/index/brand06_spic5.jpg",
+		"name1": "Fresh",
+		"name2": "馥蕾诗保湿修护唇膜（柠檬...",
+		"price": "￥150.00"
+	},
+	{
+		"src": "img/index/brand06_spic6.jpg",
+		"name1": "Fresh",
+		"name2": "馥蕾诗保湿修护唇膜（柠檬...",
+		"price": "￥150.00"
+	},
+	{
+		"src": "img/index/brand06_spic7.jpg",
+		"name1": "Fresh",
+		"name2": "馥蕾诗保湿修护唇膜（柠檬...",
+		"price": "￥150.00"
+	},{
+		"src": "img/index/brand06_spic8.jpg",
+		"name1": "Fresh",
+		"name2": "馥蕾诗保湿修护唇膜（柠檬...",
+		"price": "￥150.00"
+	}]
+
+var brand07 = [{
+		"src": "img/index/brand07_spic1.jpg",
+		"name1": "PERCYANDREED",
+		"name2": "佩妍丽丰盈套装",
+		"price": "￥148.00"
+	},
+	{
+		"src": "img/index/brand07_spic2.jpg",
+		"name1": "PERCYANDREED",
+		"name2": "佩妍丽丰盈套装",
+		"price": "￥148.00"
+	},
+	{
+		"src": "img/index/brand07_spic3.jpg",
+		"name1": "PERCYANDREED",
+		"name2": "佩妍丽丰盈套装",
+		"price": "￥148.00"
+	},
+	{
+		"src": "img/index/brand07_spic4.jpg",
+		"name1": "PERCYANDREED",
+		"name2": "佩妍丽丰盈套装",
+		"price": "￥148.00"
+	},
+	{
+		"src": "img/index/brand07_spic5.jpg",
+		"name1": "PERCYANDREED",
+		"name2": "佩妍丽丰盈套装",
+		"price": "￥148.00"
+	},
+	{
+		"src": "img/index/brand07_spic6.jpg",
+		"name1": "PERCYANDREED",
+		"name2": "佩妍丽丰盈套装",
+		"price": "￥148.00"
+	},
+	{
+		"src": "img/index/brand07_spic7.jpg",
+		"name1": "PERCYANDREED",
+		"name2": "佩妍丽丰盈套装",
+		"price": "￥148.00"
+	},
+	{
+		"src": "img/index/brand07_spic8.jpg",
+		"name1": "PERCYANDREED",
+		"name2": "佩妍丽丰盈套装",
+		"price": "￥148.00"
+	}]
+
+var like = [{
+		"src": "img/index/like_img1.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img2.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img3.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img4.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img5.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img6.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img7.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img8.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},	
+	{
+		"src": "img/index/like_img9.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img10.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img11.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img12.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img13.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img14.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img15.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img16.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img17.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img18.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img19.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	},
+	{
+		"src": "img/index/like_img20.jpg",
+		"name1": "LANCOM",
+		"name2": "兰蔻眼部精华肌底液嘉年华礼盒",
+		"price": "￥680.00"
+	}]
+var details = [{
+		"src": "img/detail/detail_product1.jpg",
+		"name1": "SEPHORACOLLECTION",
+		"name2": "丝芙兰快乐小萌猪玩趣小猪蹄十色眼影盘",
+		"price": "259"
+}]
+
+var car = [{
+		"src1": "img/shoppingCar/shoppingbag_lunbo1.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻眼部精华肌底液嘉年华...",
+		"price": "￥￥680.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo2.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥凝脂恒久气...",
+		"price": "￥570.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo3.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻肌底精华眼霜惠选套装",
+		"price": "￥520.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo4.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻水份缘舒悦凝霜惠选...",
+		"price": "￥575.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo5.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻肌底修护舒润精华...",
+		"price": "￥690.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo5.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻肌底修护舒润精华...",
+		"price": "￥690.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo4.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻水份缘舒悦凝霜惠选...",
+		"price": "￥575.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo3.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻肌底精华眼霜惠选套装",
+		"price": "￥520.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo2.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥凝脂恒久气...",
+		"price": "￥570.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo1.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻眼部精华肌底液嘉年华...",
+		"price": "￥￥680.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo1.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻眼部精华肌底液嘉年华...",
+		"price": "￥￥680.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo2.jpg",
+		"name1": "DIOR",
+		"name2": "克丽丝汀迪奥凝脂恒久气...",
+		"price": "￥570.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo3.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻肌底精华眼霜惠选套装",
+		"price": "￥520.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo4.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻水份缘舒悦凝霜惠选...",
+		"price": "￥575.00"
+	},
+	{
+		"src1": "img/shoppingCar/shoppingbag_lunbo5.jpg",
+		"name1": "LANCOME",
+		"name2": "兰蔻肌底修护舒润精华...",
+		"price": "￥690.00"
+	}]
+
+var listBrand = [{
+		"src": "img/list/list_img0.jpg",
+		"name": "丝芙兰大眼随身眼影盘",
+		"price": "￥398.00"
+	},
+	{
+		"src": "img/list/list_img1.jpg",
+		"name": "丝芙兰牛油果海藻蚕丝面膜6盒惠选套装（“小海牛”）",
+		"price": "￥398.00"
+	},
+	{
+		"src": "img/list/list_img2.jpg",
+		"name": "丝芙兰莲花珍珠蚕丝面膜6盒惠选套装",
+		"price": "￥399.00"
+	},
+	{
+		"src": "img/list/list_img3.jpg",
+		"name": "丝芙兰漫威英雄彩妆刷套装",
+		"price": "￥398.00"
+	},
+	{
+		"src": "img/list/list_img4.jpg",
+		"name": "丝芙兰牛油果石榴蚕丝面膜6盒惠选",
+		"price": "￥239.00"
+	},
+	{
+		"src": "img/list/list_img5.jpg",
+		"name": "丝芙兰快乐小萌猪化妆刷套装",
+		"price": "￥69.00"
+	},
+	{
+		"src": "img/list/list_img4.jpg",
+		"name": "丝芙兰牛油果石榴蚕丝面膜6盒惠选",
+		"price": "￥239.00"
+	},
+	{
+		"src": "img/list/list_img1.jpg",
+		"name": "丝芙兰莲花珍珠蚕丝面膜6盒惠选套装",
+		"price": "￥398.00"
+	},
+	{
+		"src": "img/list/list_img2.jpg",
+		"name": "丝芙兰牛油果石榴蚕丝面膜6盒惠选",
+		"price": "￥399.00"
+	},
+	{
+		"src": "img/list/list_img3.jpg",
+		"name": "丝芙兰牛油果石榴蚕丝面膜6盒惠选",
+		"price": "￥398.00"
+	},
+	{
+		"src": "img/list/list_img0.jpg",
+		"name": "丝芙兰牛油果海藻蚕丝面膜6盒惠选套装（“小海牛”）",
+		"price": "￥398.00"
+	},
+	{
+		"src": "img/list/list_img1.jpg",
+		"name": "丝芙兰莲花珍珠蚕丝面膜6盒惠选套装",
+		"price": "￥398.00"
+	},
+	{
+		"src": "img/list/list_img2.jpg",
+		"name": "丝芙兰漫威英雄彩妆刷套装",
+		"price": "￥399.00"
+	},
+	{
+		"src": "img/list/list_img3.jpg",
+		"name": "丝芙兰牛油果石榴蚕丝面膜6盒惠选",
+		"price": "￥398.00"
+	},
+	{
+		"src": "img/list/list_img4.jpg",
+		"name": "丝芙兰快乐小萌猪化妆刷套装（含不倒翁",
+		"price": "￥239.00"
+	},
+	{
+		"src": "img/list/list_img5.jpg",
+		"name": "丝芙兰大眼随身眼影盘",
+		"price": "￥69.00"
+	},
+	{
+		"src": "img/list/list_img4.jpg",
+		"name": "丝芙兰快乐小萌猪化妆刷套装（含不倒翁",
+		"price": "￥239.00"
+	},
+	{
+		"src": "img/list/list_img1.jpg",
+		"name": "丝芙兰莲花珍珠蚕丝面膜6盒惠选套装",
+		"price": "￥398.00"
+	},
+	{
+		"src": "img/list/list_img2.jpg",
+		"name": "丝芙兰漫威英雄彩妆刷套装",
+		"price": "￥399.00"
+	},
+	{
+		"src": "img/list/list_img3.jpg",
+		"name": "丝芙兰牛油果石榴蚕丝面膜6盒惠选",
+		"price": "￥398.00"
+	},
+	{
+		"src": "img/list/list_img5.jpg",
+		"name": "丝芙兰大眼随身眼影盘",
+		"price": "￥69.00"
+	},
+	{
+		"src": "img/list/list_img4.jpg",
+		"name": "丝芙兰快乐小萌猪化妆刷套装（含不倒翁",
+		"price": "￥239.00"
+	},
+	{
+		"src": "img/list/list_img1.jpg",
+		"name": "丝芙兰莲花珍珠蚕丝面膜6盒惠选套装",
+		"price": "￥398.00"
+	},
+	{
+		"src": "img/list/list_img2.jpg",
+		"name": "丝芙兰漫威英雄彩妆刷套装",
+		"price": "￥399.00"
+	},
+	{
+		"src": "img/list/list_img3.jpg",
+		"name": "丝芙兰牛油果石榴蚕丝面膜6盒惠选",
+		"price": "￥398.00"
+	}]
